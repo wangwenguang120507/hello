e690e4f5e58454e53db96e60e9d8cf1fc662efa7
wangwenguang120507 <bunkou.ou@pmb.rand.co.jp>
2022-07-13 17:31:35 +0800

    コメント

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HelloController {
   @RequestMapping("/hello")
   public String hello(Model model) {
-    model.addAttribute("hello", "Hello World!");
+    model.addAttribute("hello", "Hello Worldテスト!");
     return "Hello";
   }
 }
